86142779ea585f6ff6637697910fc54dd3067ed5
Seleznev_DV <directumrxuser@c.ru>
2023-12-13 17:03:44 +0300

    Разработаны роли «Cотрудники с правом создавать замещения на себя», «Ответственные за настройку замещений для подразделения».

--- DirRX.SubstitutionsManagment/DirRX.SubstitutionsManagment.Server/ModuleInitializer.cs
+++ DirRX.SubstitutionsManagment/DirRX.SubstitutionsManagment.Server/ModuleInitializer.cs
@@ -10,6 +10,27 @@ namespace DirRX.SubstitutionsManagment.Server
   public partial class ModuleInitializer
   {
 
+    public override void Initializing(Sungero.Domain.ModuleInitializingEventArgs e)
+    {
+      CreateRoles();
+    }
+    
+    public static void CreateRoles()
+    {
+      // Cотрудники с правом создавать замещения на себя.
+      Sungero.Docflow.PublicInitializationFunctions.Module.CreateRole(DirRX.SubstitutionsManagment.Resources.SubstitutionManagerRoleName,
+                                                                      DirRX.SubstitutionsManagment.Resources.SubstitutionManagerRoleDescription,
+                                                                      DirRX.SubstitutionsManagment.PublicConstants.Module.RoleGuid.SubstitutionManager);
+      // Ответственные за настройку замещений для подразделения.
+      Sungero.Docflow.PublicInitializationFunctions.Module.CreateRole(DirRX.SubstitutionsManagment.Resources.DepartmentSubstitutionManagerRoleName,
+                                                                      DirRX.SubstitutionsManagment.Resources.DepartmentSubstitutionManagerRoleDescription,
+                                                                      DirRX.SubstitutionsManagment.PublicConstants.Module.RoleGuid.DepartmentSubstitutionManager);
+    }
+    
+    /// <summary>
+    /// Видимость модуля в проводнике.
+    /// </summary>
+    /// <returns>Признак видимости модуля.</returns>
     public override bool IsModuleVisible()
     {
       return true;
--- DirRX.SubstitutionsManagment/DirRX.SubstitutionsManagment.Shared/ModuleConstants.cs
+++ DirRX.SubstitutionsManagment/DirRX.SubstitutionsManagment.Shared/ModuleConstants.cs
@@ -5,6 +5,22 @@ namespace DirRX.SubstitutionsManagment.Constants
 {
   public static class Module
   {
-
+    /// <summary>
+    /// Идентификаторы ролей.
+    /// </summary>
+    [Public]
+    public static class RoleGuid
+    {
+      /// <summary>
+      /// Cотрудники с правом создавать замещения на себя.
+      /// </summary>
+      [Public]
+      public static readonly Guid SubstitutionManager = Guid.Parse("1DAC6D4F-9502-44A7-8872-1E8F4449B007");
+      /// <summary>
+      /// Ответственные за настройку замещений для подразделения.
+      /// </summary>
+      [Public]
+      public static readonly Guid DepartmentSubstitutionManager = Guid.Parse("5D81B15B-CDEC-423F-BC3C-7A1EF4415340");
+    }
   }
 }
